5c8f8c24e5cde34c94d90feb71d39d11052bc34e
mahesh-muttinti <maheshmuttinti@gmail.com>
2021-02-04 18:12:45 +0530

    initial-commit

---
+++ _helpers/db.js
@@ -0,0 +1,16 @@
+const config = require('config.json')
+const mongoose = require('mongoose')
+const connectionOptions = {
+	useCreateIndex: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false
+}
+
+mongoose.connect(process.env.MONGO_URI || config.connectionString, connectionOptions)
+
+mongoose.Promise = global.Promise
+
+module.exports = {
+	User: require('../users/user.model')
+}
